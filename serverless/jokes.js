129eb081be6daefa36d6afe42dc2a4cda84282ed
Kaweesi Matia <matiakaweesi@gmail.com>
2024-05-08 08:19:33 +0300

    add code

---
+++ serverless/jokes.js
@@ -0,0 +1,41 @@
+// const fetch=require('node-fetch')
+// exports.handler=async(event,context)=>{
+//     const url='https://icanhazdadjoke.com'
+//     try{
+//         const jokeStream=await fetch(url,{
+//             headers:{
+//                 Accept:"application/json"
+//             }
+//         })
+//         const jsonJoke=await jokeStream.json()
+//         return {
+//             statusCode:200,
+//             body:JSON.stringify(jsonJoke)
+//         }
+//     }catch(err){
+//        return {statusCode:422,body:err.stack}
+//     }
+// }
+
+
+exports.handler = async (event, context) => {
+    const url = 'https://icanhazdadjoke.com';
+    try {
+        const fetch = await import('node-fetch'); // Dynamic import
+        const jokeStream = await fetch.default(url, {
+            headers: {
+                Accept: "application/json"
+            }
+        });
+        const jsonJoke = await jokeStream.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(jsonJoke)
+        };
+    } catch (err) {
+        return {
+            statusCode: 422,
+            body: err.stack
+        };
+    }
+};
